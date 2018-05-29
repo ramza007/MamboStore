312b749fda636dc471587662b502a200ac488b27
ramza007 <ramza007@gmail.com>
2018-05-29 09:59:36 +0300

    added first template

--- mambo/settings.py
+++ mambo/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'storeapp.apps.StoreappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
