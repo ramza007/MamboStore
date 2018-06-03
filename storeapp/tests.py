51783633927d560ab9933579d88e1500c752954e
ramza007 <ramza007@gmail.com>
2018-06-03 21:23:23 +0300

    added test classes

--- storeapp/tests.py
+++ storeapp/tests.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from .models import Profile
-#  Image, Comment
+from .models import Profile, Image, Comment
 
 # Create your tests here.
 
@@ -44,3 +43,141 @@ class ProfileTestClass(TestCase):
         self.test_profile = Profile(user=self.jane, bio="Another Profile")
         gotten_profiles = Profile.get_other_profiles(self.james.id)
         profiles = Profile.objects.all()
+
+
+class ImageTestClass(TestCase):
+    '''
+    Test case for the Image class
+    '''
+
+    def setUp(self):
+        '''
+        Method that creates an instance of Profile class
+        '''
+        # Create a Image instance
+        self.new_Image = Image(
+            caption='Python James is Muriuki who wrote Python content for Moringa School')
+
+    def test_instance(self):
+        '''
+        Test case to check if self.new_Image in an instance of Image class
+        '''
+        self.assertTrue(isinstance(self.new_Image, Image))
+
+    def test_get_Images(self):
+        '''
+        Test case to check if all Images are gotten from the database
+        '''
+        gotten_images = Image.get_Images()
+
+        images = Image.objects.all()
+
+        self.assertTrue(len(gotten_Images) == len(images))
+
+    def test_get_profile_Images(self):
+        '''
+        Test case to check if all Images for a specific profile are gotten from the database
+        '''
+        self.james = User(username="kiki")
+        self.james.save()
+
+        self.jane = User(username="ja-ne")
+        self.jane.save()
+
+        self.test_profile = Profile(user=self.jane, bio="Another Profile")
+
+        self.test_image = Image(user=self.jane, caption="Another Profile")
+
+        gotten_profile = Image.get_profile_images(self.jane.id)
+
+        profiles = Image.objects.all()
+
+        self.assertTrue(len(gotten_profile) == len(profiles))
+
+
+class FollowTestClass(TestCase):
+    '''
+    Test case for the Follow class
+    '''
+
+    def test_instance(self):
+        '''
+        Test case to check if self.new_Image in an instance of Follow class
+        '''
+        self.james = User(username="kiki")
+        self.james.save()
+
+        self.jane = User(username="ja-ne")
+        self.jane.save()
+
+        self.test_profile = Profile(user=self.jane, bio="Another Profile")
+
+        self.new_follow = Follow(user=self.jane, profile=self.test_profile)
+
+        self.assertTrue(isinstance(self.new_follow, Follow))
+
+    def test_get_following(self):
+        '''
+        Test case to check if get following is getting profiles a specific user is following
+        '''
+        self.james = User(username="kiki")
+        self.james.save()
+
+        self.jane = User(username="ja-ne")
+        self.jane.save()
+
+        self.test_profile = Profile(user=self.jane, bio="Another Profile")
+
+        self.test_Image = Image(user=self.jane, caption="Another Profile")
+
+        self.new_follow = Follow(user=self.jane, profile=self.test_profile)
+
+        gotten_following = Follow.get_following(self.jane.id)
+
+        followings = Follow.objects.all()
+
+        self.assertTrue(len(gotten_following) == len(followings))
+
+
+class CommentTestClass(TestCase):
+    '''
+    Test case for the Comment class
+    '''
+
+    def setUp(self):
+        '''
+        Method that creates an instance of Comment class
+        '''
+        # Create a Comment instance
+        self.new_comment = Comment(
+            comment_content='Python James is Muriuki who wrote Python content for Moringa School')
+
+    def test_instance(self):
+        '''
+        Test case to check if self.new_comment in an instance of Comment class
+        '''
+        self.assertTrue(isinstance(self.new_comment, Comment))
+
+    def test_get_Image_comments(self):
+        '''
+        Test case to check if get Image comments is getting comments for a specific Image
+        '''
+        self.james = User(username="kiki")
+        self.james.save()
+
+        self.jane = User(username="ja-ne")
+        self.jane.save()
+
+        self.test_profile = Profile(user=self.jane, bio="Another Profile")
+
+        self.test_Image = Image(user=self.jane, caption="Another Profile")
+
+        self.test_comment = Comment(
+            Image=self.test_Image, comment_content="Wow")
+
+        gotten_comments = Comment.get_Image_comments(self.test_Image.id)
+
+        comments = Comment.objects.all()
+
+        # No comments were saved so expect True
+        self.assertTrue(len(gotten_comments) == len(comments))
