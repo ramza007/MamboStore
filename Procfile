46401d53ae977a587ed9f12ffd0e4f19836d1f77
ramza007 <ramza007@gmail.com>
2018-06-03 21:39:48 +0300

    added procfile, runtime and requirements files

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn mambo.wsgi --log-file -
--- mambo/settings.py
+++ mambo/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'mambo.urls'
@@ -123,6 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+import django_heroku
+django_heroku.settings(locals())
